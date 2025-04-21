dc6112e1bc5b7c6d0c4adae6776694125b397398
Ilya Maltsev <malcevilya5@gmail.com>
2025-04-22 02:09:46 +0300

    feat: dirty untested version of scc based on use of adjacency list

--- src/main/kotlin/model/algo/scc.kt
+++ src/main/kotlin/model/algo/scc.kt
@@ -1,15 +1,20 @@
 package model.algo
 import graph.*
+import java.util.SortedSet
+import kotlin.math.max
 
-fun Graph.find_scc() {
+fun Graph.find_scc(): MutableSet<MutableSet<Vertex>> {
     if (!this.directed) {
         throw Exception("Can't perform search of strongly connected components on not directed graph")
     }
 
     fun dfs_with_timer(g: Map<Vertex, Set<Pair<Vertex, Double>>>, u: Vertex, timer: Int,
-                       timer_storage: MutableMap<Vertex, Int>) {
-        g[u]?.forEach { dfs_with_timer(g, it.first, timer + 1, timer_storage) }
-        timer_storage[u] = timer
+                       timer_storage: MutableMap<Vertex, Int>): Int {
+        var vertex_timer = timer
+        g[u]?.forEach { if (timer_storage[it.first] == 0)
+                        vertex_timer = max(dfs_with_timer(g, it.first, vertex_timer, timer_storage), vertex_timer) }
+        timer_storage[u] = vertex_timer + 1
+        return vertex_timer + 1
     }
 
     fun invert_edges(g: Graph): Graph {
@@ -22,4 +27,28 @@ fun Graph.find_scc() {
         return inverted
     }
 
+    fun dfs_to_build_components(g: Map<Vertex, Set<Pair<Vertex, Double>>>, u: Vertex, component: MutableSet<Vertex>) {
+        g[u]?.forEach { component.add(it.first); dfs_to_build_components(g, it.first, component) }
+    }
+
+    val source_graph_timer_storage = this.vertices.associateWith { 0 }.toMutableMap()
+    val source_graph = this.get_adjacency_list()
+    var source_graph_timer = 1
+    this.vertices.forEach { vertex -> if (source_graph_timer_storage[vertex] == 0) source_graph_timer =
+                            dfs_with_timer(source_graph, vertex, source_graph_timer, source_graph_timer_storage) }
+
+
+    val traversable_vertices = source_graph_timer_storage.keys.toSortedSet(compareBy
+                                                { source_graph_timer_storage[it] })
+    val inverted_graph = invert_edges(this).get_adjacency_list()
+    val strongly_connected_components = mutableSetOf<MutableSet<Vertex>>()
+    while (!traversable_vertices.isEmpty()) {
+        val another_vertex = traversable_vertices.last()
+        val new_component = mutableSetOf<Vertex>(another_vertex)
+        dfs_to_build_components(inverted_graph, another_vertex, new_component)
+        new_component.forEach { traversable_vertices.remove(it) }
+        strongly_connected_components.add(new_component)
+    }
+
+    return strongly_connected_components
 }
