d3df606c9023aef6f8a69ec66357738521de781a
Ilya Maltsev <malcevilya5@gmail.com>
2025-05-03 19:35:46 +0300

    feat: removed support of multigraphs, adding an edge between two already connected vertices doesnt cause exception now

--- src/main/kotlin/model/graph/Graph.kt
+++ src/main/kotlin/model/graph/Graph.kt
@@ -1,6 +1,6 @@
 package graph
 
-class Graph(val directed: Boolean, val weighted: Boolean, val multigraph: Boolean) {
+class Graph(val directed: Boolean, val weighted: Boolean) {
     internal val vertices: MutableSet<Vertex> = mutableSetOf()
     internal val edges: MutableMap<Vertex, MutableSet<Edge>> = mutableMapOf()
 
@@ -8,20 +8,18 @@ class Graph(val directed: Boolean, val weighted: Boolean, val multigraph: Boolea
         vertices.add(v)
     }
 
-    internal fun add_edge(u: Vertex, v: Vertex, w: Double = 1.0) {
-        if (!this.multigraph && ((edges[u])?.any { it.from == u && it.to == v } == true)) {
-            throw Exception("Such edge between vertices already exists")
+    internal fun add_edge(u: Vertex, v: Vertex, w: Double = 1.0): Boolean {
+        if ((edges[u])?.any { it.to == v } == true) {
+            return false
         }
         edges[u]?.add(Edge(u, v, w)) ?: edges.put(u, mutableSetOf(Edge(u, v, w)))
         if (!this.directed) {
             edges[v]?.add(Edge(v, u, w)) ?: edges.put(v, mutableSetOf(Edge(v, u, w)))
         }
+        return true
     }
 
     internal fun get_adjacency_list(): Map<Vertex, Set<Pair<Vertex, Double>>> {
-        if (this.multigraph) {
-            TODO()
-        }
         val edge_to_target_vertex: (Edge) -> Pair<Vertex, Double> = { e: Edge -> Pair(e.to, e.weight) }
         // можно использовать !!, так как изначально используем ключи из MutableMap
         return edges.keys.associateBy({vertex -> vertex }, { vertex ->
