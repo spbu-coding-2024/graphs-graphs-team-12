334f77afdb5dcde6a3d05dc4367aba38d402f6fa
Ilya Maltsev <malcevilya5@gmail.com>
2025-05-14 05:46:40 +0300

    fix: minor fixes due to formatter messages

--- src/main/kotlin/view/buttons/StartUpButtons.kt
+++ src/main/kotlin/view/buttons/StartUpButtons.kt
@@ -7,16 +7,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import view.window
 
 @Composable
 fun startUpButton(text: String) {
-    val open_win = remember { mutableStateOf(false) }
-    Button(onClick = { open_win.value = true }, modifier = Modifier
-        .fillMaxWidth()) {
+    val openWin = remember { mutableStateOf(false) }
+    Button(
+        onClick = { openWin.value = true },
+        modifier =
+            Modifier
+                .fillMaxWidth(),
+    ) {
         Text(text)
     }
-    if (open_win.value) {
-        window(open_win)
+    if (openWin.value) {
     }
 }
--- src/main/kotlin/view/layouts/StartUpLayout.kt
+++ src/main/kotlin/view/layouts/StartUpLayout.kt
@@ -8,26 +8,32 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
-import view.buttons.startUpText
 import view.buttons.startUpButton
+import view.buttons.startUpText
 
 @Composable
 fun startUpLayout() {
-    BoxWithConstraints( modifier = Modifier
-        .fillMaxSize()) {
+    BoxWithConstraints(
+        modifier =
+            Modifier
+                .fillMaxSize(),
+    ) {
         val desiredWidth = Dp((maxWidth.value * 0.5).toFloat())
         val xOffset = Dp((maxWidth.value * 0.25).toFloat())
         val yOffset = Dp((maxHeight.value * 0.33).toFloat())
 
-        Column( modifier = Modifier
-            .width(desiredWidth)
-            .offset(x = xOffset,
-                    y = yOffset)
-        )
-        {
+        Column(
+            modifier =
+                Modifier
+                    .width(desiredWidth)
+                    .offset(
+                        x = xOffset,
+                        y = yOffset,
+                    ),
+        ) {
             startUpText("Выберите, каким образом хотите загрузить граф:")
             startUpButton("SQLite database")
-            startUpButton("Neo4j databse")
+            startUpButton("Neo4j database")
             startUpButton("JSON")
         }
     }
